e3e2e60f69c14fd7a26b7a4a18cdf41f9e5a31cd
OwenpE2 <155679269+OwenpE2@users.noreply.github.com>
2024-01-05 04:02:28 -0500

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -80,6 +80,14 @@ handler.all = async function(m, {conn}) {
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
 
+    if (!chat.isBanned && m.text.match(/(mi primera chamba)/gi)) {
+    if (!db.data.chats[m.chat].audios) return;
+    if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
+    const vn = './media/mi primera chamba.mp3';
+    mconn.conn.sendPresenceUpdate('recording', m.chat);
+    mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+  }
+  
   if (!chat.isBanned && m.text.match(/(fino senores|sino señores|fino)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
